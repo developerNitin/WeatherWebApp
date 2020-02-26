dbe2f24aedfdc4854b546efc51398262bcee67c2
Nitin Birdi <nitin.birdi@icloud.com>
2020-02-26 13:23:48 +0530

    new commit

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+//jshint esversion: 6
 
-var express = require("express");
-var https = require("https");
-var bodyParser = require("body-parser");
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
 
-var app = express();
+const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -13,19 +14,19 @@ app.use(bodyParser.urlencoded({extended: true}));
  });
 
 app.post("/", function(req, res) {
-  var query = req.body.cityName;
-  var apikey = "aa2dc135995e9722da2d1c7ae9dcff7a";
-  var units = "metric";
-  var url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apikey + "&units=" + units;
+  const query = req.body.cityName;
+  const apikey = "aa2dc135995e9722da2d1c7ae9dcff7a";
+  const units = "metric";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apikey + "&units=" + units;
 
   https.get(url, function(response) {
 
     response.on("data", function(data) {
-      var weatherData = JSON.parse(data);
-      var temp = weatherData.main.temp;
-      var weatherDescription = weatherData.weather[0].description;
-      var icon = weatherData.weather[0].icon;
-      var imageURL = "http://openweathermap.org/img/wn/" + icon +"@2x.png";
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imageURL = "http://openweathermap.org/img/wn/" + icon +"@2x.png";
 
       res.write( "<p>the weather is" + " currently " + weatherDescription + "</p>");
       res.write("<h1>The temp in " + query + " is " + temp + " degress Celcius</h1>");
